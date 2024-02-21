e3442eb5d37c638a0c4fff191f7b1321a5d6d870
Rodrigopt <rodrigopte23@gmail.com>
2024-02-21 16:33:56 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/favicon.ico 	modified:   src/app/globals.css 	modified:   src/app/layout.tsx 	modified:   src/app/page.module.css 	modified:   src/app/page.tsx 	public/background.jpg 	public/logo.jpg 	public/logoPrevenet.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,102 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+//import './globals.css'
+import { Inter, REM, WindSong } from 'next/font/google'
+import Image from 'next/image';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'PreveNet',
+  description: 'Distribuidor de Tarefas Preventivas',
+}
+
+export function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+        <Image
+          className='bg-image-wrapper'
+          src="/background.jpg"
+          alt="Background"
+          width={1920}
+          height={1080}
+          quality={100}
+        />
+      <div className="card" style={{ width: '24rem', marginRight: 'auto', marginLeft: 'auto', marginTop: '30px', marginBottom: 'auto' }}>
+        <div style={{ margin: '20px', imageOrientation:'center'}}>
+          <Image src="/logoPrevenet.png" className="img-fluid" alt="..." width={400} height={100} objectPosition='' style={{}}/>
+          <div className="card-body">
+            <p className="card-text" style={{ textAlign: 'center' }}>Sistema de Distribuição de Preventivas Automatizado</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <form>
+              <fieldset>
+                <div className="mb-3">
+                  <label htmlFor="selectRegional" className="form-label">Tipo de Preventiva</label>
+                  <select id="selectRegional" className="form-select">
+                    <option value="1">Preventiva Site</option>
+                    <option value="2">Preventiva Rede BackBone</option>
+                    <option value="3">Preventiva CTO</option>
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="disabledSelect" className="form-label">Regional</label>
+                  <select id="disabledSelect" className="form-select">
+                    <option value="1">Regional Central</option>
+                    <option value="2">Regional Campinas</option>
+                    <option value="3">Regional Centro-Oeste</option>
+                    <option value="4">Regional Vale-Sul</option>
+                  </select>
+                </div>
+              </fieldset>
+              <fieldset disabled>
+                <div className="mb-3">
+                  <label htmlFor="disabledSelect" className="form-label">Cidade</label>
+                  <select id="disabledSelect" className="form-select">
+                    <option value="1"></option>
+                  </select>
+                </div>
+              </fieldset>
+              <fieldset disabled>
+                <div className="mb-3">
+                  <label htmlFor="kmRede" className="form-label">Km</label>
+                  <input type="text" className="form-control" id="kmRede" />
+                </div>
+              </fieldset>
+              <fieldset disabled>
+                <div className="mb-3">
+                  <label htmlFor="disabledSelect" className="form-label">Site</label>
+                  <select id="disabledSelect" className="form-select">
+                    <option value="1"></option>
+                  </select>
+                </div>
+              </fieldset>
+              <fieldset disabled>
+                <div className="mb-3">
+                  <label htmlFor="disabledSelect" className="form-label">Técnico</label>
+                  <select id="disabledSelect" className="form-select">
+                    <option value="1"></option>
+                  </select>
+                </div>
+                </fieldset>
+                <div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+          </ul>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </main >
+  )
 }
